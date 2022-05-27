00ce712b9cc6ffbe1ed80ddd95757c7fce3dd38e
Casey Mau <mauza@mauza.net>
2022-05-26 19:56:32 -0600

    went to requirements.txt

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import pychromecast
 import logging
 import os
 import sys
 import RPi.GPIO as GPIO
 import time
-from dotenv import load_dotenv
-
-load_dotenv()
+import utils
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
+
+# SMS Vars
+recipients = os.getenv("RECIPIENTS")
 
 # Chromecast Variables
 chromecast_name = os.getenv("CHROMECAST_NAME")
 mp3 = os.getenv("MP3_URL")
 
 # Setting up pin for button
-pin = os.getenv("BUTTON_PIN")
+pin = int(os.getenv("BUTTON_PIN"))
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 logging.info("Starting up chromecasts")
+time.sleep(1)
 chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[chromecast_name])
+time.sleep(1)
 cast = chromecasts[0]
 cast.wait()
 
@@ -38,6 +44,10 @@ def main():
         if input_state == False:
             play_mp3(mp3)
             logging.info("Button Pressed")
+            #picture = utils.take_pic()
+            #logging.info("Picture Taken")
+            #utils.send_text("Someone is at the door Door DOOR", picture, recipients)
+            #logging.info("Text Sent")
             time.sleep(0.5)
     browser.stop_discovery()
 
--- utils.py
+++ utils.py
@@ -1,22 +1,48 @@
 import os
 import smtplib
+import io
+import logging
+import cv2
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from picamera2 import Picamera2
+from PIL import Image
+
+logger = logging.getLogger()
 
 def take_pic():
+    picam2 = Picamera2()
+    config = picam2.still_configuration()
+    picam2.configure(config)
+    picam2.start()
+    np_image = picam2.capture_array()
+    is_success, im_buf_arr = cv2.imencode(".png", np_image)
+    picam2.stop()
+    return im_buf_arr.tobytes()
 
-
-def send_text(msg_text, recipients):
+def send_text(msg_text, img_data, recipients):
     # Create message
-    msg = MIMEText(msg_text)
+    msg = MIMEMultipart()
+    text = MIMEText(msg_text)
+    msg.attach(text)
+    image = MIMEImage(img_data, name="porch_photo.jpg")
+    msg.attach(image)
     msg['From'] = os.environ.get('SMTP_USER')
     msg['To'] = 'Awesomeness'
-
     # Create server object with SSL option
     server = smtplib.SMTP_SSL('smtp.zoho.com', 465)
 
     # Perform operations via server
     server.login(os.environ.get('SMTP_USER'), os.environ.get('SMTP_PASSWORD'))
-    server.sendmail(os.environ.get('SMTP_USER'), recipients, msg.as_string())
+    server.sendmail(os.environ.get('SMTP_USER'), recipients.split(','), msg.as_string())
     server.quit()
+
+def main():
+    recipients = "3852196527@mailmymobile.net"
+    png = take_pic()
+    send_text("test", png, recipients)
+
+if __name__ == "__main__":
+    main()
 
